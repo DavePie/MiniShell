04f8b6b6a16221193569b0d31bb0e0cce4393766
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2023-11-10 13:54:20 +0100

    updated .h and started instr

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: dvandenb <dvandenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 13:25:21 by dvandenb          #+#    #+#             */
-/*   Updated: 2023/11/10 13:47:18 by dvandenb         ###   ########.fr       */
+/*   Updated: 2023/11/10 13:53:14 by dvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,24 @@ int	ewrite(char *s)
 	write(0, "minishell: ", ft_strlen("minishell: "));
 	write(0, s, ft_strlen(s));
 	write(0, "\n", ft_strlen("\n"));
+}
+
+int	instr(char *s, int i)
+{
+	char	cur;
+
+	cur = 0;
+	while (*s && i)
+	{
+		i--;
+		if (*s == '"' || *s == '\'')
+		{
+			if (cur == *s)
+				cur = 0;
+			else if (!cur)
+				cur = *s;
+		}
+		s++;
+	}
+	return (!cur);
 }
--- utils.h
+++ utils.h
@@ -6,7 +6,7 @@
 /*   By: dvandenb <dvandenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:08:31 by dvandenb          #+#    #+#             */
-/*   Updated: 2023/11/10 13:32:01 by dvandenb         ###   ########.fr       */
+/*   Updated: 2023/11/10 13:50:39 by dvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,22 @@
  * @param i the index
  * @return int 1 if single quote, 2 if double quote
  */
-int		instr(char *s, int i);
+int	instr(char *s, int i);
 
+/**
+ * @brief Given a string, return its length
+ * 
+ * @param s the string
+ * @return int (its length)
+ */
 int	ft_strlen(const char *s);
 
-int		ewrite(char *s);
+/**
+ * @brief Given an error message, write it in the correct format
+ * @note minishell: <error-message>
+ * @param s 
+ * @return int 
+ */
+int	ewrite(char *s);
 
 #endif
