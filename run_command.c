abfee5c1c524d3579e51cfe29dbed31850a85418
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2023-11-20 11:31:21 +0100

    added descriptors for test run function

--- run_command.c
+++ run_command.c
@@ -6,7 +6,7 @@
 /*   By: dvandenb <dvandenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 11:07:11 by dvandenb          #+#    #+#             */
-/*   Updated: 2023/11/20 11:24:22 by dvandenb         ###   ########.fr       */
+/*   Updated: 2023/11/20 11:30:22 by dvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,29 @@ int	run(char *str, int start, int end, char **envp)
 	t_token	**f_list;
 
 	f_list = malloc(sizeof(t_token *));
+
 	input = ft_substr(str, start, end - start);
+	printf("input string: %s", input);
+
+	printf("split args:\n");
 	f_list = split_args(input);
 	print_tokens(*f_list);
-	
+
+	printf("add env:\n");
 	ft_convert_envs(f_list, envp);
 	print_tokens(*f_list);
-	
+
+	printf("pre-merge wildcard:\n");
 	merge_tokens(*f_list, 0);
 	print_tokens(*f_list);
-	
+
+	printf("wildcard:\n");
 	convert_wildcards(f_list);
 	print_tokens(*f_list);
-	
+
+	printf("final merge:\n");
 	merge_tokens(*f_list, 1);
 	print_tokens(*f_list);
-	
+
 	return ((start - end) % 2);
 }
