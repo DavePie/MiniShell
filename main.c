52c7dbce4d2aada1c09e1ed65da23e8619ab9eed
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2023-11-28 16:18:28 +0100

    added temp change

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dvandenb <dvandenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 09:35:09 by dvandenb          #+#    #+#             */
-/*   Updated: 2023/11/28 13:18:47 by dvandenb         ###   ########.fr       */
+/*   Updated: 2023/11/28 14:21:29 by dvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	main(int ac, char *av[], char **envp)
 	while (1)
 	{
 		d.input = ft_read_line();
-		if (!d.input)
+		if (!d.input || !strcmp(d.input, "exit"))
 			exit_shell(0, 0);
 		if (verify_input(d.input))
 			run_all_commands(&d, envp);
